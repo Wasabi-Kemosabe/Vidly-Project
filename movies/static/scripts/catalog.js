7c5b0aa8b9f4e0fb21ec15e2e3cd962f4847a09e
Angelo Totanes <hazukashi_gariyasan79@yahoo.com>
2019-10-27 22:18:54 -0700

    Added modals to each movie information

--- movies/static/scripts/catalog.js
+++ movies/static/scripts/catalog.js
@@ -7,7 +7,28 @@ function displayMovie(movie) {
         <div class="card-body">
             <h5 class="card-title">${movie.title}</h5>
             <p class="card-text">${movie.genre.name}</p>
-            <a href="#" class="btn btn-primary">Click here for details!</a>
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#movie-modal">
+                Click here for more info!
+            </button>
+            <div class="modal fade" id="movie-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            ...
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
     `;
